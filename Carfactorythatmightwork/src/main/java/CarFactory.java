88228ea0942db63548aadf17855396a110e7aab3
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-12-26 21:01:12 +0100

    Add files via upload

--- Carfactorythatmightwork/src/main/java/CarFactory.java
+++ Carfactorythatmightwork/src/main/java/CarFactory.java
@@ -2,17 +2,38 @@ package main.java;
 
 public class CarFactory {
 
-    public Car getCar(String brand) throws CarException {
-        switch(brand) {
+    private String[] brands = {"Audi", "Bmw"};
+    private String[] engines = {"Engine1", "Engine2"};
+
+    private String getAlphaNumericString() {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+        StringBuilder sb = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    public Car getCar(String color) throws CarException {
+        switch(brands[0]) {
             case "Audi": {
-                Audi audi = new Audi("Black");
-                System.out.println(audi.getRegistrationNumber());
-                return audi;
+                Car Audi = new Car(color);
+                Audi.setRegNumber(getAlphaNumericString());
+                System.out.println(Audi.getRegNumber());
+                Audi.setBrand(brands[0]);
+                Audi.setEngine(engines[0]);
+                return Audi;
             }
             case "Bmw": {
-                Bmw bmw = new Bmw("Green");
-                System.out.println(bmw.getRegistrationNumber());
-                return bmw;
+                Car Bmw = new Car(color);
+                Bmw.setRegNumber(getAlphaNumericString());
+                System.out.println(Bmw.getRegNumber());
+                Bmw.setBrand(brands[0]);
+                Bmw.setEngine(engines[0]);
+                return Bmw;
             }
             default: {
                 throw new CarException("Not a valid brand");

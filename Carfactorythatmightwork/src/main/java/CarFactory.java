7a9d221f65a23583edb1407c378743e542d8875a
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-12-26 21:09:29 +0100

    Add files via upload
    Final touches, Cleaner code, Making the new Carfactory look like my first one that i hade to remake 3times

--- Carfactorythatmightwork/src/main/java/CarFactory.java
+++ Carfactorythatmightwork/src/main/java/CarFactory.java
@@ -2,41 +2,51 @@ package main.java;
 
 public class CarFactory {
 
-    private String[] brands = {"Audi", "Bmw"};
-    private String[] engines = {"Engine1", "Engine2"};
+    private String[] brands = {"Audi", "BMW"};
+    private String[] engines = {"V4", "V5"};
 
-    private String getAlphaNumericString() {
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
-        StringBuilder sb = new StringBuilder(6);
+    private String Regplates() {
+        String Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+        StringBuilder lettergenerator = new StringBuilder(3);
+        StringBuilder numbergenerator = new StringBuilder(3);
 
-        for (int i = 0; i < 6; i++) {
-            int index = (int)(AlphaNumericString.length() * Math.random());
-            sb.append(AlphaNumericString.charAt(index));
+        for (int i = 0; i < 3; i++) {
+            int index = (int)(Letters.length() * Math.random());
+            lettergenerator.append(Letters.charAt(index));
         }
+        for (int i = 0; i < 3; i++) {
+            int index = (int)(numbers.length() * Math.random());
+            numbergenerator.append(numbers.charAt(index));
+        }
+        StringBuilder regPlate = new StringBuilder(6);
+        regPlate.append(lettergenerator);
+        regPlate.append(numbergenerator);
 
-        return sb.toString();
+        return regPlate.toString();
     }
 
     public Car getCar(String color) throws CarException {
         switch(brands[0]) {
-            case "Audi": {
-                Car Audi = new Car(color);
-                Audi.setRegNumber(getAlphaNumericString());
-                System.out.println(Audi.getRegNumber());
-                Audi.setBrand(brands[0]);
-                Audi.setEngine(engines[0]);
-                return Audi;
+            case "BMW": {
+                Car bmw = new Car(color);
+                bmw.setRegNumber(Regplates());
+                System.out.println(bmw.getRegNumber());
+                bmw.setBrand(brands[0]);
+                bmw.setEngine(engines[0]);
+                return bmw;
             }
-            case "Bmw": {
-                Car Bmw = new Car(color);
-                Bmw.setRegNumber(getAlphaNumericString());
-                System.out.println(Bmw.getRegNumber());
-                Bmw.setBrand(brands[0]);
-                Bmw.setEngine(engines[0]);
-                return Bmw;
+            case "Audi": {
+                Car audi = new Car(color);
+                audi.setRegNumber(Regplates());
+                System.out.println(audi.getRegNumber());
+                audi.setBrand(brands[0]);
+                audi.setEngine(engines[0]);
+                return audi;
             }
             default: {
                 throw new CarException("Not a valid brand");
             }
+        }
+    }
 }
-    }}
--- Carfactorythatmightwork/src/test/CarFactoryTest.java
+++ Carfactorythatmightwork/src/test/CarFactoryTest.java
@@ -11,19 +11,36 @@ public class CarFactoryTest {
     CarFactory carFactory = new CarFactory();
 
     @Test
-    public void test_create_car_success() throws CarException { Car car = carFactory.getCar("Red");assertNotNull(car);}
-
+    public void test_create_car_success() throws CarException {
+        Car car = carFactory.getCar("Red");
+        assertNotNull(car);
+    }
     @Test
     public void test_get_car_color_success() throws CarException {
-        assertEquals("Red", carFactory.getCar("Red").getColor());
-        assertEquals("White", carFactory.getCar("White").getColor());
-        assertEquals("Blue", carFactory.getCar("Blue").getColor());
+        assertEquals("green", carFactory.getCar("green").getColor());
     }
-    
     @Test
-    public void test_get_audi_car_brand_success() throws CarException { assertEquals("Audi", carFactory.getCar("Audi").getBrand()); }
-
+    public void test_get_car_color_fail() throws CarException {
+        assertEquals("purple", carFactory.getCar("pink").getColor());
+    }
     @Test
-    public void test_get_engine_success() throws CarException { assertEquals("Engine1", carFactory.getCar("Blue").getEngine()); }
-
+    public void test_get_engine_success() throws CarException {
+        assertEquals("V4", carFactory.getCar("white").getEngine());
+    }
+    @Test
+    public void test_get_engine_fail() throws CarException {
+        assertEquals("V6", carFactory.getCar("pink").getEngine());
+    }
+    @Test
+    public void test_get_car_brand_success() throws CarException {
+        assertEquals("Audi", carFactory.getCar("red").getBrand());
+    }
+    @Test
+    public void test_get_car_brand_fail() throws CarException {
+        assertEquals("Porsche", carFactory.getCar("red").getBrand());
+    }
+    @Test
+    public void test_get_car_reg_fail() throws CarException {
+        assertEquals("ABC123", carFactory.getCar("white").getRegNumber());
+    }
 }

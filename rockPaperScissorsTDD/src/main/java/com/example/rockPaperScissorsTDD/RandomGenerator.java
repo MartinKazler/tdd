ed7cb21c46eba53713386d739ce53899e981d0fc
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-12-26 23:01:45 +0100

    Real base.....
    Here we go again....

---
+++ rockPaperScissorsTDD/src/main/java/com/example/rockPaperScissorsTDD/RandomGenerator.java
@@ -0,0 +1,12 @@
+package com.example.rockPaperScissorsTDD;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+        Random rand = new Random();
+
+        public int getRandom() {
+            return rand.nextInt(3);
+        }
+    }

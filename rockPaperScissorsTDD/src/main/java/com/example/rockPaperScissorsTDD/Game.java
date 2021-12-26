3aaae4874629195005aade492d1cc6ee1b2e9391
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-12-26 23:23:45 +0100

    Add files via upload

---
+++ rockPaperScissorsTDD/src/main/java/com/example/rockPaperScissorsTDD/Game.java
@@ -0,0 +1,87 @@
+package com.example.rockPaperScissorsTDD;
+
+
+import java.util.Scanner;
+
+public class Game {
+    private final Rock rock = new Rock();
+    private final Scissors scissors = new Scissors();
+    private final Paper paper = new Paper();
+    private GesturesFactory gesturesFactory = new GesturesFactory();
+    private final GameCounter counter = new GameCounter();
+
+    public Game() {
+    }
+
+    public void startGame() {
+        System.out.println("Please enter your name");
+        Scanner playerScanner = new Scanner(System.in);
+
+        String name = playerScanner.nextLine();
+        Player player = new Player(name);
+        Player cpu = new Player("Cpu");
+
+        System.out.println("You have chosen the name " + player.getName());
+        System.out.println("The game begins now.\nChoose between ROCK, PAPER and SCISSOR");
+        boolean gameLoop = true;
+        while (gameLoop) {
+            String playerChoice = playerScanner.nextLine().toUpperCase();
+
+            gameLogic(player, cpu, playerChoice);
+
+            System.out.println(player.getName() + " score: " + counter.getPlayerPoints());
+            System.out.println(cpu.getName() + " score: " + counter.getComputerPoints());
+
+            if (counter.getPlayerPoints() == 3){
+                gameLoop = false;
+                System.out.println(player.getName() + " wins!");
+            }
+            if (counter.getComputerPoints() == 3){
+                gameLoop = false;
+                System.out.println(cpu.getName() + " wins!");
+            }
+        }
+    }
+    private void gameLogic(Player player, Player cpu, String playerChoice) {
+        String cpuChoice = cpuChoice();
+        System.out.println("cpu choice " + cpuChoice);
+
+
+        if ((playerChoice.equals(cpuChoice))) {
+            System.out.println("Draw!");
+        } else if ((playerChoice.equals("ROCK")) && (cpuChoice.equals("SCISSORS"))) {
+            if (rock.beats(scissors)) {counter.playerWon();}
+        } else if ((playerChoice.equals("SCISSORS")) && (cpuChoice.equals("PAPER"))) {
+            if (scissors.beats(paper)) { counter.playerWon(); }
+        } else if ((playerChoice.equals("PAPER")) && (cpuChoice.equals("ROCK"))) {
+            if(paper.beats(rock)){ counter.playerWon(); }
+        }else if ((cpuChoice.equals("ROCK")) && (playerChoice.equals("SCISSORS"))){
+            if(rock.beats(scissors)){ counter.computerWon(); }
+        }else if ((cpuChoice.equals("SCISSORS")) && (playerChoice.equals("PAPER"))){
+            if(scissors.beats(paper)){ counter.computerWon(); }
+        }else if ((cpuChoice.equals("PAPER")) && (playerChoice.equals("ROCK"))){
+            if(paper.beats(rock)){ counter.computerWon(); }
+        }
+
+    }
+
+    private String cpuChoice() {
+        RandomGenerator randomGenerator = new RandomGenerator();
+        int choice = randomGenerator.getRandom();
+        switch (choice) {
+            case 0:
+                return "ROCK";
+
+            case 1:
+                return "SCISSORS";
+
+            case 2:
+                return "PAPER";
+
+            default:
+                return "RIP";
+
+        }
+    }
+
+}
--- rockPaperScissorsTDD/src/test/java/com/example/rockPaperScissorsTDD/RockPaperScissorsTest.java
+++ rockPaperScissorsTDD/src/test/java/com/example/rockPaperScissorsTDD/RockPaperScissorsTest.java
@@ -2,68 +2,74 @@ package com.example.rockPaperScissorsTDD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RockPaperScissorsTest {
+
     Rock rock;
     Paper paper;
     Scissors scissors;
+    GameCounter counter;
+
     @BeforeEach
     void setUp() {
         rock = new Rock();
         paper = new Paper();
         scissors = new Scissors();
+        counter = new GameCounter();
     }
 
-    // Rock tests
+
 
     @Test
     void rock_beats_scissors_success() {
         assertTrue(rock.beats(scissors));
+
     }
 
     @Test
     void rock_beats_paper_fail() {
         assertFalse(rock.beats(paper));
     }
-
     @Test
     void rock_beats_rock_fail() {
         assertFalse(rock.beats(rock));
     }
 
-    // Paper tests
-
     @Test
     void paper_beats_scissors_fail() {
         assertFalse(paper.beats(scissors));
     }
-
     @Test
     void paper_beats_paper_fail() {
         assertFalse(paper.beats(paper));
     }
-
     @Test
     void paper_beats_rock_success() {
         assertTrue(paper.beats(rock));
     }
 
-    // Scissors tests
-
     @Test
     void scissors_beats_scissors_fail() {
         assertFalse(scissors.beats(scissors));
     }
-
     @Test
     void scissors_beats_paper_success() {
         assertTrue(scissors.beats(paper));
     }
-
     @Test
     void scissors_beats_rock_fail() {
         assertFalse(scissors.beats(rock));
+    }
+
+    @Test
+    void test_player_wins_3_to_0_vs_cpu() {
+        assertTrue(scissors.beats(paper));
+
+        assertTrue(scissors.beats(paper));
+
+        assertTrue(scissors.beats(paper));
+
     }
 }
